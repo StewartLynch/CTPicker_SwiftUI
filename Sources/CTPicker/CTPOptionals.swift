e9f2172969a96017fdf89ff58122b38a927301d4
Florian Schweizer <post.an.flo@gmail.com>
2021-12-04 09:11:46 +0100

    Make CTPColors and CTPStrings public

--- Sources/CTPicker/CTPOptionals.swift
+++ Sources/CTPicker/CTPOptionals.swift
@@ -12,14 +12,14 @@ import UIKit
 
 public enum CTPColors {
     // Default values
-    static var headerBackgroundColor: UIColor = .darkGray
-    static var headerTintColor: UIColor = .white
+    public static var headerBackgroundColor: UIColor = .darkGray
+    public static var headerTintColor: UIColor = .white
 }
 
 public enum CTPStrings {
     // Default values
-    static var pickText: String = "Filter list then tap entry to select."
-    static var addText: String = "Filter list then tap entry to select, or type new entry then '+' to add."
-    static var searchPlaceHolder: String = "Filter by entering text..."
-    static var cancelButtonTitle: String =  "Cancel"
+    public static var pickText: String = "Filter list then tap entry to select."
+    public static var addText: String = "Filter list then tap entry to select, or type new entry then '+' to add."
+    public static var searchPlaceHolder: String = "Filter by entering text..."
+    public static var cancelButtonTitle: String =  "Cancel"
 }
